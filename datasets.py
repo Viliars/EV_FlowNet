a04842662d6db81b090563f10519c5f5afe16ffa
Viliars <redkillerpw@mail.ru>
2020-03-29 22:20:32 +0300

    2 version train.py

--- datasets.py
+++ datasets.py
@@ -3,6 +3,7 @@ import h5py
 from random import randint
 import numpy as np
 
+
 class KITTY(torch.utils.data.Dataset):
     def __init__(self, path):
         super(KITTY).__init__()
@@ -15,17 +16,28 @@ class KITTY(torch.utils.data.Dataset):
         self.len = sum(self.length)
 
     def __getitem__(self, idx):
+        for i in range(92):
+            if idx - self.length[i] > 0:
+                idx -= self.length[i]
+            else:
+                file_id = i
+                break
+        file = h5py.File(self.path / "{:010d}.hdf5".format(file_id), "r")
+
         h = randint(0, 256)
         w = randint(0, 1136)
-        pred_image = torch.Tensor(self.file['pred'][idx][h:h + 256, w:w + 256].reshape(1, 256, 256))
-        next_image = torch.Tensor(self.file['next'][idx][h:h + 256, w:w + 256].reshape(1, 256, 256))
+
+        pred_image = torch.Tensor(file['pred'][idx][h:h + 256, w:w + 256].reshape(1, 256, 256))
+        next_image = torch.Tensor(file['next'][idx][h:h + 256, w:w + 256].reshape(1, 256, 256))
 
         event_image = torch.Tensor(np.stack([
-            self.file['0'][idx][h:h + 256, w:w + 256],
-            self.file['1'][idx][h:h + 256, w:w + 256],
-            self.file['2'][idx][h:h + 256, w:w + 256],
-            self.file['3'][idx][h:h + 256, w:w + 256]
+            file['0'][idx][h:h + 256, w:w + 256],
+            file['1'][idx][h:h + 256, w:w + 256],
+            file['2'][idx][h:h + 256, w:w + 256],
+            file['3'][idx][h:h + 256, w:w + 256]
         ]))
+
+        file.close()
 
         return pred_image, next_image, event_image
 
--- train.py
+++ train.py
@@ -1,12 +1,18 @@
 from pathlib import Path
 import torch
+import numpy as np
 from net import Model
 from datasets import KITTY
+from tqdm import tqdm
+from loss import photometric_loss, smoothness_loss
+from torch.utils.tensorboard import SummaryWriter
 
 path = Path("/hpcfs/GRAPHICS2/23m_pri/data/")
 
 dataset = KITTY(path)
 dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=16, num_workers=1, pin_memory=True)
+
+writer = SummaryWriter()
 
 device = torch.device('cuda:0')
 model = Model()
@@ -15,25 +21,29 @@ imsize = 256, 256
 
 print(len(dataset))
 
-# model.train()
-#
-# optimizer = torch.optim.Adam(model.parameters(), lr=1.0e-5)
-#
-# losses = []
-# images = []
-# for epoch in range(30):
-#     for i_batch, sample_batched in tqdm(enumerate(dataset_loader)):
-#         optimizer.zero_grad()
-#
-#         pred_images, next_images, event_images = sample_batched
-#
-#         pred_images.to(device)
-#         next_images.to(device)
-#         event_images.to(device)
-#
-#         flow = model.forward(event_images)
-#         loss = photometric_loss(pred_images, next_images, flow) + 0.5 * smoothness_loss(flow)
-#
-#         loss.backward()
-#         optimizer.step()
+model.train()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=1.0e-5)
+
+for epoch in range(30):
+    print(f"------ EPOCH {epoch} ------")
+    losses = []
+    for i_batch, sample_batched in tqdm(enumerate(dataset_loader)):
+        optimizer.zero_grad()
+
+        pred_images, next_images, event_images = sample_batched
+
+        pred_images.to(device)
+        next_images.to(device)
+        event_images.to(device)
+
+        flow = model.forward(event_images)
+        loss = photometric_loss(pred_images, next_images, flow) + 0.5 * smoothness_loss(flow)
+
+        loss.backward()
+        optimizer.step()
+
+        losses.append(loss.item())
+
+    writer.add_scalar('Loss/train', np.average(losses), epoch)
 
